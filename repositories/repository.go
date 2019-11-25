37a983d52b9515b1f8e63bb9fc950c81748288d4
Tsovak <tsovak36@gmail.com>
2019-11-25 14:48:24 +0300

    add PaymentRepository interface

--- repositories/repository.go
+++ repositories/repository.go
@@ -22,3 +22,16 @@ type AccountRepository interface {
 	// Update balance for given account by incrementing on given value
 	UpdateBalance(ctx context.Context, id string, incr int64) error
 }
+
+// PaymentRepository declare repository for payments
+type PaymentRepository interface {
+
+	// GetAll return all payments
+	GetAll(ctx context.Context) ([]model.Payment, error)
+
+	// Store save new payment
+	Store(ctx context.Context, payment *model.Payment) error
+
+	// Find Payment by id
+	FindById(ctx context.Context, id string) (model.Payment, error)
+}
