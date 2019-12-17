82994652d75bbe7483455ecfcdcf7fed91b0a34a
Tsovak <tsovak36@gmail.com>
2019-12-17 14:57:38 +0300

    add Request and Response types

--- api/model/types.go
+++ api/model/types.go
@@ -12,7 +12,35 @@ type Account struct {
 type Payment struct {
 	ID            int64  `db:"id"`
 	Amount        int64  `db:"amount"`
-	ToAccountID   string `db:"to_account_id" json:"to_id" `
-	FromAccountID string `db:"from_account_id" json:"from_id"`
+	ToAccountID   string `db:"to_account_id" `
+	FromAccountID string `db:"from_account_id"`
 	Direction     string `db:"direction"`
+}
+
+type AccountRequest struct {
+	Name     string `json:"name"`
+	Currency string `json:"currency"`
+	Balance  int64  `json:"balance"`
+}
+
+type PaymentRequest struct {
+	Amount        int64  `json:"amount"`
+	ToAccountID   string `json:"to_id" `
+	FromAccountID string `json:"from_id"`
+	Direction     string `json:"direction"`
+}
+
+type AccountResponse struct {
+	ID       int64  `json:"id"`
+	Name     string `json:"name"`
+	Currency string `json:"currency"`
+	Balance  int64  `json:"balance"`
+}
+
+type PaymentResponse struct {
+	ID            int64  `json:"id"`
+	Amount        int64  `json:"amount"`
+	ToAccountID   string `json:"to_account" `
+	FromAccountID string `json:"from_account"`
+	Direction     string `json:"direction"`
 }
